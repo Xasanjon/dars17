dfd0c16a019a5f8847c45579cdcb009285a25acd
Akaxon <xasanboy99akaxon@gmail.com>
2021-08-31 01:09:53 +0500

    reset password

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,17 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+# EMAIL BACKEND
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# DEFAULT_FROM_EMAIL = 'admin@sariq.dev'
+# EMAIL_HOST = 'smpt.sariq.dev'
+# EMAIL_HOST_USER ='username'
+# EMAIL_HOST_PASSWORD = 'password'
+# EMAIL_POSR = 587
+# EMAIL_USE_TLS = True
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
